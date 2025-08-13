2220ec9834bb880dfde55f7b79ace66a9423276e
Meendy <manumeendy@gmail.com>
2025-08-13 00:17:28 -0600

    Refactorizar notificaciones

---
+++ app/Enums/NotificationType.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Enums;
+
+enum NotificationType: string
+{
+    case SUCCESS = 'success';
+    case ERROR = 'error';
+    case WARNING = 'warning';
+    case INFO = 'info';
+
+    public static function labels(): array
+    {
+        return [
+            self::SUCCESS->value => 'Éxito',
+            self::ERROR->value => 'Error',
+            self::WARNING->value => 'Advertencia',
+            self::INFO->value => 'Información',
+        ];
+    }
+}
