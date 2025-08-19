08412e6f2563702e26866d24a94decdd24c7ab0d
Meendy <manumeendy@gmail.com>
2025-08-18 23:27:11 -0600

    fix: Update UserSeeder to remove unnecessary user entries and ensure consistent email verification

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,35 +14,20 @@ class UserSeeder extends Seeder
     {
         User::create([
             'email' => 'admin@example.com',
+            'email_verified_at' => now(),
             'password' => Hash::make('password'),
             'name' => 'Admin',
             'id_institution' => 1,
             "type" => TypeUser::ADMIN->value
-            //Poner siempre el de mayusculas
         ]);
 
         User::create([
-            'email' => 'student@example.com',
+            'email' => 'Are271@hotmail.com',
+            'email_verified_at' => now(),
             'password' => Hash::make('password'),
-            'name' => 'Student',
-            'id_institution' => 2,
-            'type' => TypeUser::STUDENT->value
-        ]);
-
-        User::create([
-            'email' => 'buyer@example.com',
-            'password' => Hash::make('password'),
-            'name' => 'Buyer',
-            'id_institution' => 3,
-            'type' => TypeUser::STUDENT->value
-        ]);
-
-        User::create([
-            'email' => 'embh2910@gmail.com',
-            'password' => Hash::make('1234'),
-            'name' => 'Ruben Ramirez Hernandez',
-            'id_institution' => 4,
-            'type' => TypeUser::STUDENT->value
+            'name' => 'Areli Admin',
+            'id_institution' => 1,
+            "type" => TypeUser::ADMIN->value
         ]);
     }
 }
